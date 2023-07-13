823cc002cd03f20ba2f96db1ccea19bdb10c335c
isiji <blairisiji@gmail.com>
2023-07-13 06:09:17 -0700

    correction

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
  * main-prints out the given line
+ *
  * return:if the value is (0) then its a success
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
