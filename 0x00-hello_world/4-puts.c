a7eff90364e5708a856f5b01fa33cb7058a6d447
isiji <blairisiji@gmail.com>
2023-07-13 04:59:46 -0700

    compilation

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that uses puts function to print a line
+ * return: Returns (0) if succesfull
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
