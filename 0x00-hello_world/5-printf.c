c79bdf3485864f7a3d191703a2c40b3127da4334
isiji <blairisiji@gmail.com>
2023-07-13 06:13:46 -0700

    correction

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,6 +5,7 @@
  * return:if the value is (0) then its a success
  */
 int main(void)
+	/*this where the process begins*/
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
