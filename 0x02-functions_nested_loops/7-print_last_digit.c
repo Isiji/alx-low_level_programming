a65b6bc35db117dd84198c0f2353a39bb4ca8fa7
isiji <blairisiji@gmail.com>
2023-07-18 14:59:59 -0700

    lastdigit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_last_digit - prints the last digit of a number
  * @n: the int to extract the last digit from
@@ -10,16 +11,7 @@ int print_last_digit(int n)
 
 	a = n % 10;
 
-	:if (a < 0)
-	{
-		_putchar(-a + 48);
-		return (-a);
-	}
-	else
-	{
-
 	_putchar(a + '0');
-	return (a);
-	}
-}
 
+	return (a);
+}
