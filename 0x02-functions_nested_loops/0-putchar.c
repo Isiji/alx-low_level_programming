480642e204b096220475659c6d28f7a54dc34923
isiji <blairisiji@gmail.com>
2023-07-18 09:25:24 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - main entry of a function that outputs a string
+ *
+ * Return: 0 (success)
+ * used my first local made header
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	return (0);
+}
