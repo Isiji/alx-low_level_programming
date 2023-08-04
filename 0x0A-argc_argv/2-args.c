e5be48d034fa2d376116dd370ab38e205509df28
isiji <blairisiji@gmail.com>
2023-08-04 06:20:51 -0700

    all arguments

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints arguments
+ * @argc: counts arguments
+ * @argv: stores arguments
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+
+}
