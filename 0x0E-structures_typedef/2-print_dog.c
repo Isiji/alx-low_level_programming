1e7d48e6923997a034ca64524c11f09c97ed5c46
isiji <blairisiji@gmail.com>
2023-08-14 19:44:03 +0000

    new dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - prints struct dog
+ * @d: pointer to struct dog
+ * @name: pointer to name of the dog
+ * @age: where age is stored
+ * @owner: where owners name is stored
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		printf("Name: %s\n", (d->name) ? d->name : "(nil)");
+		printf("Age: %f\n", (d->age) ? d->age : 0);
+		printf("Owner: %s\n", (d->owner) ? d->age : "(nil)");
+	}
+}
