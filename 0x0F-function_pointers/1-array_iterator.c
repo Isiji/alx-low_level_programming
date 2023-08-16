7818be5c2d5de6803b5b4b701a6737c0efd33f2b
isiji <blairisiji@gmail.com>
2023-08-16 16:39:22 +0000

    iterator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -2,9 +2,8 @@
 #include "function_pointers.h"
 
 /**
- * array-iterator - iterates through an array
+ * array_iterator - iterates through an array
  * @array: where elements are stored
- * @size_t: measures the size of the array
  * @size: the actual size of the array
  * @action: the function to be executed
  */
