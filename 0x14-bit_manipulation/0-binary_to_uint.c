c470ab895f0602b891df09666326d5635f5f55c9
isiji <blairisiji@gmail.com>
2023-08-31 17:27:04 +0000

    binary

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,25 +9,22 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int num = 0;
-	int len, i;
 
-	if(!b)
+	if (!b)
 		return (0);
 
-	len = strlen(b);
-
-	for (i = 0; i < len; i++)
+	while (*b)
 	{
-		if (*b == '0')
-			num <<= 1;
-
-		else if (*b == '1')
+		if (*b == '0' || *b == '1')
 		{
 			num <<= 1;
-			num |= 1;
+			num |= (*b - '0');
 		}
 		else
+		{
 			return (0);
+		}
+		b++;
 
 	}
 	return (num);
